cbeaf4afd5007196c6407544c4f0cbd22d3aeec4
Dhananajaya <dhananjay.m1805@gmailcom>
2025-03-16 20:51:28 +0530

    inital setup for the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BoardView from "./Components/BoardView";
+
+function App() {
+  return (
+    <div className="App">
+      <BoardView />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BoardView.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BoardView = () => {};
+
+export default BoardView;
